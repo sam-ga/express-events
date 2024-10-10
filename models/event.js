d0cddfc5cf2a401c82d42908a5571a14de5df407
Sam <sharrisdev@gmail.com>
2024-10-10 14:39:58 +0200

    updated referenced fields

--- models/event.js
+++ models/event.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const commentSchema = new mongoose.Schema({
   text: { type: String, required: true },
-  user: { type: mongoose.Types.ObjectId, ref: 'User', required: true }
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
 }, {
   timestamps: true // this option set to true provides a dynamic createdAt and updatedAt field that update automatically
 })
@@ -12,9 +12,9 @@ const eventSchema = new mongoose.Schema({
   description: { type: String, required: ['Add a description', true] },
   location: { type: String, required: ['Add a location', true] },
   date: { type: Date, required: ['Add a date', true] },
-  organiser: { type: mongoose.Types.ObjectId, ref: 'User', required: true },
+  organiser: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   comments: [commentSchema],
-  attendees: [{ type: mongoose.Types.ObjectId, ref: 'User'}],
+  attendees: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
   images: [String]
 })
 
